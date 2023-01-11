1c56e9588ca0a545768b1a4a0535febec3200b5a
Lucas Nobre <lucasnobre37@outlook.com>
2023-01-11 19:55:05 -0300

    update index menu style

--- pages/styles.jsx
+++ pages/styles.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const MobileMenu = styled.nav`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
   height: 80px;
   background: linear-gradient(180deg, #032520 0%, #111d28 100%, #0b131b 100%);
