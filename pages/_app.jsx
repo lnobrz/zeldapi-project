5dd8e262c1cd0dc095bad81b31e53b47dee03d81
Lucas Nobre <lucasnobre37@outlook.com>
2023-01-12 02:45:40 -0300

    add MobileHeader and Footer components to _app.jsx file

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,15 @@
 import { GlobalStyle } from "./globalStyles";
 import { bitter, kadwa, hyliaSerif } from "../public/fonts/fonts";
+import MobileHeader from "../components/MobileHeader/MobileHeader";
+import Footer from "../components/Footer/Footer";
 
 const App = ({ Component, pageProps }) => {
   return (
     <div className={[bitter.className, kadwa.className, hyliaSerif.className]}>
       <GlobalStyle />
-      <Component {...pageProps} />;
+      <MobileHeader />
+      <Component {...pageProps} />
+      <Footer />
     </div>
   );
 };
