5f1832cffa5ffe168e67bd1da85d1bfd5de02750
Lucas Nobre <lucasnobre37@outlook.com>
2023-01-12 01:32:20 -0300

    create storage.js file to store dummy data

---
+++ storage/storage.js
@@ -0,0 +1,23 @@
+export const mainText =
+  "I am Lucas Nobre and I’m here to show you my frontend programming skills and, for it, I'll take you to a trip into the universe of The Legend of Zelda.";
+
+export const menuCategories = [
+  {
+    id: 1,
+    title: "characters",
+    subtitle: "meet",
+    url: "/characters",
+  },
+  {
+    id: 2,
+    title: "monsters",
+    subtitle: "face",
+    url: "/monsters",
+  },
+  {
+    id: 3,
+    title: "places",
+    subtitle: "discover",
+    url: "/places",
+  },
+];
