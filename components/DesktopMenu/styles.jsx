347e8c6a98eb8c66ec791025d5733b95458d60ff
Lucas Nobre <lucasnobre37@outlook.com>
2023-01-14 03:25:38 -0300

    create DesktopMenu component

---
+++ components/DesktopMenu/styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MenuContainer = styled.ul`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  margin-right: 60px;
+`;
