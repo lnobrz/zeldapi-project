b899070819f2527953a6e171816cf904607a2756
Lucas Nobre <lucasnobre37@outlook.com>
2023-01-16 11:37:28 -0300

    add links and logo to footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,18 +5,40 @@ import {
   FooterTitle,
   RightsContainer,
 } from "./styles";
-import Logo from "../Logo/Logo";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <BackgroundContainer>
       <FooterContainer>
-        <Logo />
+        <Image
+          src="/illustrations/dark-theme-logo.svg"
+          alt="zeldapi logo"
+          width={100}
+          height={40}
+          style={{ marginTop: "40px" }}
+        />
         <FooterTitle>Liked my job? Contact me!</FooterTitle>
         <ul>
-          <ContactItem>lucasnobre37@outlook.com</ContactItem>
-          <ContactItem>https://github.com/lnobrz</ContactItem>
-          <ContactItem>https://www.linkedin.com/in/lucas-nobrz/</ContactItem>
+          <ContactItem>
+            <Link href="mailto:lucasnobre37@outlook.com" className="footerLink">
+              lucasnobre37@outlook.com
+            </Link>
+          </ContactItem>
+          <ContactItem>
+            <Link href="https://github.com/lnobrz" className="footerLink">
+              https://github.com/lnobrz
+            </Link>
+          </ContactItem>
+          <ContactItem>
+            <Link
+              href="https://www.linkedin.com/in/lucas-nobrz/"
+              className="footerLink"
+            >
+              https://www.linkedin.com/in/lucas-nobrz/
+            </Link>
+          </ContactItem>
         </ul>
         <RightsContainer>
           Lucas Nobre, 2023. Some Rights Reserved.
